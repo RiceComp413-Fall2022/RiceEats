128cdf773b5be3a7d409b2903ec458d4dcd9834e
John Lee <jl170@rice.edu>
2022-09-25 17:58:02 -0500

    database setup complete

--- back_end/django_server/main/admin.py
+++ back_end/django_server/main/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import FullReview, ReviewItem
+from .models import Reviewer, Servery, MenuItem, MealType,FullReview, ReviewItem, Reviewer, Servery, MenuItem, MealType, Meal, MenuItemServed
 
 # Register your models here.
 admin.site.register(FullReview)
 admin.site.register(ReviewItem)
+admin.site.register(Reviewer)
+admin.site.register(Servery)
+admin.site.register(MenuItem)
+admin.site.register(MealType)
+admin.site.register(Meal)
+admin.site.register(MenuItemServed)
--- back_end/django_server/main/models.py
+++ back_end/django_server/main/models.py
@@ -1,17 +1,64 @@
 from django.db import models
 
 # Create your models here.
-class FullReview(models.Model):
-    name = models.CharField(max_length=200)
+class Reviewer(models.Model):
+    netid = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30)
+    college = models.CharField(max_length=20)
+    numreviews = models.IntegerField()
+
+    def __str__(self):
+        return self.netid
+
+class Servery(models.Model):
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
-    
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    vegetarian = models.BooleanField()
+    glutenFree = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class MealType(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Meal(models.Model):
+    servery = models.ForeignKey(Servery, on_delete=models.CASCADE)
+    mealType = models.ForeignKey(MealType, on_delete=models.CASCADE)
+    servedDate = models.DateField()
+
+    def __str__(self):
+        return str(self.servedDate) + "_" + str(self.mealType) + "_" + str(self.servery)
+
+class MenuItemServed(models.Model):
+    menuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.menuItem.name) + " @ " + str(self.meal)
+
+class FullReview(models.Model):
+    reviewer = models.ForeignKey(Reviewer, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    reviewText = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return str(self.reviewer.netid) + ": " + str(self.meal)
 
 class ReviewItem(models.Model):
     fullReview = models.ForeignKey(FullReview, on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
+    menuItemServed = models.ForeignKey(MenuItemServed, on_delete=models.CASCADE)
+    reviewText = models.CharField(max_length=200)
     complete = models.BooleanField()
+    rating = models.IntegerField()
 
     def __str__(self):
-        return self.text
+        return str(self.fullReview.reviewer.netid) + ": " + str(self.fullReview.meal) + ", " + str(self.menuItemServed.menuItem.name)
