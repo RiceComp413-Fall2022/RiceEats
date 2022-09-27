8f45fc090e089d926953ff9fd45744c283d88bac
Joseph <jeg4@rice.edu>
2022-09-27 11:39:38 -0500

    added links

---
+++ front_end/rice-eats/src/components/CurrentMeal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CurrentMeal(props) {
+    return (
+        <div style={{fontSize:30}}>
+            The current meal is Lunch (11:30 AM - 1:30 PM).
+        </div>
+    )
+}
--- front_end/rice-eats/src/components/ServeryCard.js
+++ front_end/rice-eats/src/components/ServeryCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function MenuItem(props) {
   const name = props.name;
@@ -16,14 +18,21 @@ export default function ServeryCard(props) {
   const name = props.name;
   const overallRating = props.overallRating;
   const menuItems = props.menuItems;
+  const url = props.url;
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate(url);
+  }
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
         <Card.Title>{name} ({overallRating})</Card.Title>
         <Card.Text>
-          {menuItems.map((menuItem) => MenuItem(menuItem))}
+          <div>
+            {menuItems.map((menuItem) => MenuItem(menuItem))}
+          </div>
         </Card.Text>
-        <Button variant="primary">See More Details</Button>
+        <Button variant="primary" onClick={onButtonClick}>See More Details</Button>
       </Card.Body>
     </Card>
   );
