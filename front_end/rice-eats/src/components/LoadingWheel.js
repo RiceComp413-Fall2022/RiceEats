15fed83f387d7c9e1ee49dafc0ef98178c892702
Joseph <jeg4@rice.edu>
2022-11-02 15:51:40 -0500

    loading wheel!

--- front_end/rice-eats/src/components/LoadingWheel.js
+++ front_end/rice-eats/src/components/LoadingWheel.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Text from './Text';
+import { BeatLoader } from 'react-spinners';
 
 export default function LoadingWheel() {
   return (
     <div>
+      <BeatLoader color="#36d7b7"/>
       <Text>Loading.....</Text>
     </div>
   );
